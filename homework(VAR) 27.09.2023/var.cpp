4f74188d4fcd34078ac9c382c62615cd6b9efae6
Filippov-Nikolay <filippovnikolay195@gmail.com>
2023-09-30 16:46:29 +0300

    Реализовал перегрузку арифметических операторов: +, -, *, /, +=, -=, *=, /= Операторов сравнения: <, >, <=, >=, ==, !=
    Реализовал преобразования к типам

---
+++ homework(VAR) 27.09.2023/var.cpp
@@ -0,0 +1,554 @@
+﻿#include "var.h"
+
+using namespace std;
+
+// Второй тип, переводится к первому типу
+
+
+// Конструктор
+var::var() { }
+var::var(int element) { varInt = element; varIntActive = true; }
+var::var(double element) { varDouble = element; varDoubleActive = true; }
+var::var(string element) { varString = element; varStringActive = true; }
+var::var(const char* element) {
+	varChar = new char[strlen(element) + 1];
+	strcpy_s(varChar, strlen(element) + 1, element);
+	varString = string(varChar);
+	varStringActive = true;
+}
+
+
+// Аксессоры
+// Геттеры
+int var::GetVarInt() const { return varInt; }
+double var::GetVarDouble() const { return varDouble; }
+string var::GetVarString() const { return varString; }
+
+bool var::GetVarIntActive() const { return varIntActive; }
+bool var::GetVarDoubleActive() const { return varDoubleActive; }
+bool var::GetVarStringActive() const { return varStringActive; }
+
+
+// Сеттеры
+void var::SetVarInt(int element) { varInt = element; }
+void var::SetVarDouble(double element) { varDouble = element; }
+void var::SetVarString(string element) { varString = element; }
+
+void var::SetVarIntActive(bool status) { varIntActive = status; }
+void var::SetVarDoubleActive(bool status) { varDoubleActive = status; }
+void var::SetVarStringActive(bool status) { varStringActive = status; }
+
+
+// Операторы
+void var::operator+=(const var a) {
+	//cout << "varString: " << varString << endl;
+	//cout << "a.GetVarInt(): " << a.GetVarInt() << endl;
+
+	// Однотипные данные
+	if (varIntActive && a.GetVarIntActive())
+		varInt += a.GetVarInt();
+	else if (varDoubleActive && a.GetVarDoubleActive())
+		varDouble += a.GetVarDouble();
+	else if (varStringActive && a.GetVarStringActive())
+		varString += a.GetVarString();
+
+	// Cправа int, cлева другой тип
+	else if (varDoubleActive && a.GetVarIntActive()) {
+		varDouble += a.GetVarInt();
+
+		varDoubleActive = 1;
+		varStringActive = 0;
+		varIntActive = 0;
+	}
+	else if (varStringActive && a.GetVarIntActive()) {
+		varString += to_string(a.GetVarInt());
+
+		varDoubleActive = 0;
+		varStringActive = 1;
+		varIntActive = 0;
+	}
+	// Cправа double, cлева другой тип
+	else if (varIntActive && a.GetVarDoubleActive()) {
+		varInt += a.GetVarDouble();
+
+		varDoubleActive = 0;
+		varStringActive = 0;
+		varIntActive = 1;
+	}
+	else if (varStringActive && a.GetVarDoubleActive()) {
+		varString += to_string(a.GetVarDouble());
+
+		varDoubleActive = 0;
+		varStringActive = 1;
+		varIntActive = 0;
+	}
+	// Cправа string, cлева другой тип
+	else if (varIntActive && a.GetVarStringActive()) {
+		varInt += stoi(a.GetVarString());
+
+		varDoubleActive = 0;
+		varStringActive = 0;
+		varIntActive = 1;
+	}
+	else if (varDoubleActive && a.GetVarStringActive()) {
+		varDouble += stod(a.GetVarString());
+
+		varDoubleActive = 1;
+		varStringActive = 0;
+		varIntActive = 0;
+	}
+	else
+		cout << "Ошибка!" << endl;
+}
+void var::operator-=(const var a) {
+	// Однотипные данные
+	if (varIntActive && a.GetVarIntActive())
+		varInt -= a.GetVarInt();
+	else if (varDoubleActive && a.GetVarDoubleActive())
+		varDouble -= a.GetVarDouble();
+	else if (varStringActive && a.GetVarStringActive()) {} // От string нельзя - string
+
+	// Cправа int, cлева другой тип
+	else if (varDoubleActive && a.GetVarIntActive()) {
+		varDouble -= a.GetVarInt();
+
+		varDoubleActive = 1;
+		varStringActive = 0;
+		varIntActive = 0;
+	}
+	else if (varStringActive && a.GetVarIntActive()) {} // От string нельзя - int
+	// Cправа double, cлева другой тип
+	else if (varIntActive && a.GetVarDoubleActive()) {
+		varInt -= a.GetVarDouble();
+
+		varDoubleActive = 0;
+		varStringActive = 0;
+		varIntActive = 1;
+	}
+	else if (varStringActive && a.GetVarDoubleActive()) {} // От string нельзя - double
+
+	// Cправа string, cлева другой тип
+	else if (varIntActive && a.GetVarStringActive()) {
+		varInt -= stoi(a.GetVarString());
+
+		varDoubleActive = 0;
+		varStringActive = 0;
+		varIntActive = 1;
+	}
+	else if (varDoubleActive && a.GetVarStringActive()) {
+		varDouble -= stod(a.GetVarString());
+
+		varDoubleActive = 1;
+		varStringActive = 0;
+		varIntActive = 0;
+	}
+	else
+		cout << "Ошибка!" << endl;
+}
+void var::operator*=(const var a) {
+	if (varStringActive && a.GetVarStringActive()) {
+		char temp[200];
+		int y = 0;
+		string tempStr;
+
+		for (int i = 0; i < varString.length(); i++)
+			for (int j = 0; j < a.GetVarString().length(); j++)
+				if (varString[i] == a.GetVarString()[j])
+					temp[y++] = varString[i];
+
+		temp[y] = '\0';
+		varString = temp;
+	}
+}
+void var::operator/=(const var a) {
+	if (varStringActive && a.GetVarStringActive()) {
+		char temp[200];
+		int y = 0;
+		string tempStr;
+
+		for (int i = 0; i < varString.length(); i++)
+			for (int j = 0; j < a.GetVarString().length(); j++)
+				if (varString[i] == a.GetVarString()[j])
+					break;
+				else if (j == a.GetVarString().length() - 1)
+					temp[y++] = varString[i];
+
+		temp[y] = '\0';
+		varString = temp;
+	}
+}
+
+// Реализовать преобразование типов
+var var::operator=(const var element) {
+	// Проверка на акт. элементы
+	/*
+	if (varIntActive)
+		cout << "varIntActive" << endl;
+	if (varDoubleActive)
+		cout << "varDoubleActive" << endl;
+	if (varStringActive)
+		cout << "varStringActive" << endl;
+
+	cout << "varInt: " << varInt << endl;
+	cout << "varDouble: " << varDouble << endl;
+	cout << "varString: " << varString << endl;
+
+
+
+	if (element.GetVarIntActive())
+		cout << "element.GetVarIntActive()" << endl;
+	if (element.GetVarDoubleActive())
+		cout << "element.GetVarDoubleActive()" << endl;
+	if (element.GetVarStringActive())
+		cout << "element.GetVarStringActive()" << endl;
+
+	cout << "element.GetVarInt(): " << element.GetVarInt() << endl;
+	cout << "element.GetVarDouble(): " << element.GetVarDouble() << endl;
+	cout << "element.GetVarString(): " << element.GetVarString() << endl;
+	*/
+
+	if (!element.GetVarIntActive() && !element.GetVarDoubleActive()) {
+		// cout << "Отработка 1го" << endl;
+
+		varStringActive = 1;
+		varIntActive = 0;
+		varDoubleActive = 0;
+		varString = element.GetVarString();
+	}
+	else if (!element.GetVarDoubleActive() && element.GetVarString().length() == 0) {
+		// cout << "Отработка 2го" << endl;
+
+		varIntActive = 1;
+		varStringActive = 0;
+		varDoubleActive = 0;
+		varInt = element.GetVarInt();
+	}
+	else if (!element.GetVarIntActive() && element.GetVarString().length() == 0) {
+		// cout << "Отработка 3го" << endl;
+
+		varDoubleActive = 1;
+		varIntActive = 0;
+		varStringActive = 0;
+		varDouble = element.GetVarDouble();
+	}
+
+	return element;
+}
+
+
+// Перегрузка ввода-вывода
+ostream& operator<<(ostream& os, const var& element) {
+	if (element.GetVarIntActive())
+		os << element.GetVarInt();
+	else if (element.GetVarDoubleActive())
+		os << element.GetVarDouble();
+	else if (element.GetVarStringActive())
+		os << element.GetVarString();
+
+	return os;
+}
+istream& operator>>(istream& is, var& element) {
+	if (element.GetVarIntActive()) {
+		int temp;
+		cin >> temp;
+		element.SetVarInt(temp);
+	}
+	else if (element.GetVarDoubleActive()) {
+		double temp;
+		cin >> temp;
+		element.SetVarDouble(temp);
+	}	
+	else if (element.GetVarStringActive()) {
+		string temp;
+		cin >> temp;
+		element.SetVarString(temp);
+	}
+		
+	return is;
+}
+
+
+// Перегрузка операторов
+var operator+(const var a, const var b) {
+	// Однотипные данные
+	if (a.GetVarIntActive() && b.GetVarIntActive())
+		return a.GetVarInt() + b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarDoubleActive())
+		return a.GetVarDouble() + b.GetVarDouble();
+	else if (a.GetVarStringActive() && b.GetVarStringActive())
+		return a.GetVarString() + b.GetVarString();
+
+	// Слева int, справа другой тип
+	else if (a.GetVarIntActive() && b.GetVarDoubleActive())
+		return a.GetVarInt() + b.GetVarDouble();
+	else if (a.GetVarIntActive() && b.GetVarStringActive())
+		return a.GetVarInt() + stoi(b.GetVarString());
+
+	// Слева double, справа другой тип
+	else if (a.GetVarDoubleActive() && b.GetVarIntActive())
+		return a.GetVarDouble() + b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarStringActive())
+		return a.GetVarDouble() + stod(b.GetVarString());
+
+	// str + int
+	// Слева string, справа другой тип
+	else if (a.GetVarStringActive() && b.GetVarIntActive())
+		return a.GetVarString() + to_string(b.GetVarInt());
+	else if (a.GetVarStringActive() && b.GetVarDoubleActive())
+		return a.GetVarString() + to_string(b.GetVarDouble());
+
+	cout << "Ошибка!" << endl;
+	return 0;
+}
+var operator-(const var a, const var b) {
+	// Однотипные данные
+	if (a.GetVarIntActive() && b.GetVarIntActive())
+		return a.GetVarInt() - b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarDoubleActive())
+		return a.GetVarDouble() - b.GetVarDouble();
+	else if (a.GetVarStringActive() && b.GetVarStringActive())
+		return 0;
+
+	// Слева int, справа другой тип
+	else if (a.GetVarIntActive() && b.GetVarDoubleActive())
+		return a.GetVarInt() - b.GetVarDouble();
+	else if (a.GetVarIntActive() && b.GetVarStringActive())
+		return a.GetVarInt() - stoi(b.GetVarString());
+
+	// Слева double, справа другой тип
+	else if (a.GetVarDoubleActive() && b.GetVarIntActive())
+		return a.GetVarDouble() - b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarStringActive())
+		return a.GetVarDouble() - stod(b.GetVarString());
+
+	// str + int
+	// Слева string, справа другой тип
+	else if (a.GetVarStringActive() && b.GetVarIntActive())
+		return a.GetVarString() - to_string(b.GetVarInt());
+	else if (a.GetVarStringActive() && b.GetVarDoubleActive())
+		return a.GetVarString() - to_string(b.GetVarDouble());
+
+	cout << "Ошибка!" << endl;
+	return 0;
+}
+var operator*(const var a, const var b) {
+	if (a.GetVarStringActive() && b.GetVarStringActive()) {
+		char temp[200];
+		int y = 0;	
+
+		for (int i = 0; i < a.GetVarString().length(); i++)
+			for (int j = 0; j < b.GetVarString().length(); j++) 
+				if (a.GetVarString()[i] == b.GetVarString()[j])
+					temp[y++] = a.GetVarString()[i];
+
+		temp[y] = '\0';
+		return var(temp);
+		// cout << "TEST: " << temp << endl;
+	}
+	
+	cout << "Ошибка!" << endl;
+	return 0; // Вернуть класс через конструктор
+}
+var operator/(const var a, const var b) {
+	if (a.GetVarStringActive() && b.GetVarStringActive()) {
+		char temp[200];
+		int y = 0;
+
+		for (int i = 0; i < a.GetVarString().length(); i++) 
+			for (int j = 0; j < b.GetVarString().length(); j++) 
+				if (a.GetVarString()[i] == b.GetVarString()[j]) 
+					break;
+				else if (j == b.GetVarString().length() - 1) 
+					temp[y++] = a.GetVarString()[i];
+
+		temp[y] = '\0';
+		return var(temp);
+		// cout << "TEST: " << temp << endl;
+	}
+
+	cout << "Ошибка!" << endl;
+	return 0;
+}
+
+
+// Операторы сравнения
+bool operator<(const var a, const var b) {
+	// Однотипные данные
+	if (a.GetVarIntActive() && b.GetVarIntActive())
+		return a.GetVarInt() < b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarDoubleActive())
+		return a.GetVarDouble() < b.GetVarDouble();
+	else if (a.GetVarStringActive() && b.GetVarStringActive())
+		return a.GetVarString().length() < b.GetVarString().length();
+
+	// Слева int, справа другой тип
+	else if (a.GetVarIntActive() && b.GetVarDoubleActive())
+		return a.GetVarInt() < b.GetVarDouble();
+	else if (a.GetVarIntActive() && b.GetVarStringActive())
+		return a.GetVarInt() < b.GetVarString().length();
+
+	// Слева double, справа другой тип
+	else if (a.GetVarDoubleActive() && b.GetVarIntActive())
+		return a.GetVarDouble() < b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarStringActive())
+		return a.GetVarDouble() < b.GetVarString().length();
+
+	// Слева string, справа другой тип
+	else if (a.GetVarStringActive() && b.GetVarIntActive())
+		return a.GetVarString().length() < b.GetVarInt();
+	else if (a.GetVarStringActive() && b.GetVarDoubleActive())
+		return a.GetVarString().length() < b.GetVarDouble();
+
+	cout << "Ошибка!" << endl;
+	return 0;
+}
+bool operator>(const var a, const var b) {
+	// Однотипные данные
+	if (a.GetVarIntActive() && b.GetVarIntActive())
+		return a.GetVarInt() > b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarDoubleActive())
+		return a.GetVarDouble() > b.GetVarDouble();
+	else if (a.GetVarStringActive() && b.GetVarStringActive())
+		return a.GetVarString().length() > b.GetVarString().length();
+
+	// Слева int, справа другой тип
+	else if (a.GetVarIntActive() && b.GetVarDoubleActive())
+		return a.GetVarInt() > b.GetVarDouble();
+	else if (a.GetVarIntActive() && b.GetVarStringActive())
+		return a.GetVarInt() > b.GetVarString().length();
+
+	// Слева double, справа другой тип
+	else if (a.GetVarDoubleActive() && b.GetVarIntActive())
+		return a.GetVarDouble() > b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarStringActive())
+		return a.GetVarDouble() > b.GetVarString().length();
+
+	// Слева string, справа другой тип
+	else if (a.GetVarStringActive() && b.GetVarIntActive())
+		return a.GetVarString().length() > b.GetVarInt();
+	else if (a.GetVarStringActive() && b.GetVarDoubleActive())
+		return a.GetVarString().length() > b.GetVarDouble();
+
+	cout << "Ошибка!" << endl;
+	return 0;
+}
+bool operator<=(const var a, const var b) {
+	// Однотипные данные
+	if (a.GetVarIntActive() && b.GetVarIntActive())
+		return a.GetVarInt() <= b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarDoubleActive())
+		return a.GetVarDouble() <= b.GetVarDouble();
+	else if (a.GetVarStringActive() && b.GetVarStringActive())
+		return a.GetVarString().length() <= b.GetVarString().length();
+
+	// Слева int, справа другой тип
+	else if (a.GetVarIntActive() && b.GetVarDoubleActive())
+		return a.GetVarInt() <= b.GetVarDouble();
+	else if (a.GetVarIntActive() && b.GetVarStringActive())
+		return a.GetVarInt() <= b.GetVarString().length();
+
+	// Слева double, справа другой тип
+	else if (a.GetVarDoubleActive() && b.GetVarIntActive())
+		return a.GetVarDouble() <= b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarStringActive())
+		return a.GetVarDouble() <= b.GetVarString().length();
+
+	// Слева string, справа другой тип
+	else if (a.GetVarStringActive() && b.GetVarIntActive())
+		return a.GetVarString().length() <= b.GetVarInt();
+	else if (a.GetVarStringActive() && b.GetVarDoubleActive())
+		return a.GetVarString().length() <= b.GetVarDouble();
+
+	cout << "Ошибка!" << endl;
+	return 0;
+}
+bool operator>=(const var a, const var b) {
+	// Однотипные данные
+	if (a.GetVarIntActive() && b.GetVarIntActive())
+		return a.GetVarInt() >= b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarDoubleActive())
+		return a.GetVarDouble() >= b.GetVarDouble();
+	else if (a.GetVarStringActive() && b.GetVarStringActive())
+		return a.GetVarString().length() >= b.GetVarString().length();
+
+	// Слева int, справа другой тип
+	else if (a.GetVarIntActive() && b.GetVarDoubleActive())
+		return a.GetVarInt() >= b.GetVarDouble();
+	else if (a.GetVarIntActive() && b.GetVarStringActive())
+		return a.GetVarInt() >= b.GetVarString().length();
+
+	// Слева double, справа другой тип
+	else if (a.GetVarDoubleActive() && b.GetVarIntActive())
+		return a.GetVarDouble() >= b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarStringActive())
+		return a.GetVarDouble() >= b.GetVarString().length();
+
+	// Слева string, справа другой тип
+	else if (a.GetVarStringActive() && b.GetVarIntActive())
+		return a.GetVarString().length() >= b.GetVarInt();
+	else if (a.GetVarStringActive() && b.GetVarDoubleActive())
+		return a.GetVarString().length() >= b.GetVarDouble();
+
+	cout << "Ошибка!" << endl;
+	return 0;
+}
+bool operator==(const var a, const var b) {
+	// Однотипные данные
+	if (a.GetVarIntActive() && b.GetVarIntActive())
+		return a.GetVarInt() == b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarDoubleActive())
+		return a.GetVarDouble() == b.GetVarDouble();
+	else if (a.GetVarStringActive() && b.GetVarStringActive())
+		return a.GetVarString().length() == b.GetVarString().length();
+
+	// Слева int, справа другой тип
+	else if (a.GetVarIntActive() && b.GetVarDoubleActive())
+		return a.GetVarInt() == b.GetVarDouble();
+	else if (a.GetVarIntActive() && b.GetVarStringActive())
+		return a.GetVarInt() == b.GetVarString().length();
+
+	// Слева double, справа другой тип
+	else if (a.GetVarDoubleActive() && b.GetVarIntActive())
+		return a.GetVarDouble() == b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarStringActive())
+		return a.GetVarDouble() == b.GetVarString().length();
+
+	// Слева string, справа другой тип
+	else if (a.GetVarStringActive() && b.GetVarIntActive())
+		return a.GetVarString().length() == b.GetVarInt();
+	else if (a.GetVarStringActive() && b.GetVarDoubleActive())
+		return a.GetVarString().length() == b.GetVarDouble();
+
+	cout << "Ошибка!" << endl;
+	return 0;
+}
+bool operator!=(const var a, const var b) {
+	// Однотипные данные
+	if (a.GetVarIntActive() && b.GetVarIntActive())
+		return a.GetVarInt() != b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarDoubleActive())
+		return a.GetVarDouble() != b.GetVarDouble();
+	else if (a.GetVarStringActive() && b.GetVarStringActive())
+		return a.GetVarString().length() != b.GetVarString().length();
+
+	// Слева int, справа другой тип
+	else if (a.GetVarIntActive() && b.GetVarDoubleActive())
+		return a.GetVarInt() != b.GetVarDouble();
+	else if (a.GetVarIntActive() && b.GetVarStringActive())
+		return a.GetVarInt() != b.GetVarString().length();
+
+	// Слева double, справа другой тип
+	else if (a.GetVarDoubleActive() && b.GetVarIntActive())
+		return a.GetVarDouble() != b.GetVarInt();
+	else if (a.GetVarDoubleActive() && b.GetVarStringActive())
+		return a.GetVarDouble() != b.GetVarString().length();
+
+	// Слева string, справа другой тип
+	else if (a.GetVarStringActive() && b.GetVarIntActive())
+		return a.GetVarString().length() != b.GetVarInt();
+	else if (a.GetVarStringActive() && b.GetVarDoubleActive())
+		return a.GetVarString().length() != b.GetVarDouble();
+
+	cout << "Ошибка!" << endl;
+	return 0;
+}
---
+++ homework(VAR) 27.09.2023/var.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class var {
+	int varInt;
+	double varDouble;
+	char* varChar;
+	string varString;
+
+	bool varIntActive = false;
+	bool varDoubleActive = false;
+	bool varStringActive = false;
+public:
+	var();
+	var(int);
+	var(double);
+	var(const char*);
+	var(string);
+
+	// Акссесоры
+	// Геттеры
+	int GetVarInt() const;
+	double GetVarDouble() const;
+	string GetVarString() const;
+
+	bool GetVarIntActive() const;
+	bool GetVarDoubleActive() const;
+	bool GetVarStringActive() const;
+
+	// Сеттеры
+	void SetVarInt(int);
+	void SetVarDouble(double);
+	void SetVarString(string);
+
+	void SetVarIntActive(bool state);
+	void SetVarDoubleActive(bool state);
+	void SetVarStringActive(bool state);
+
+
+	// Операторы
+	void operator+=(const var);
+	void operator-=(const var);
+	void operator*=(const var);
+	void operator/=(const var);
+
+	var operator=(const var);
+};
+
+
+// Перегрузка ввода-вывода
+ostream& operator<<(ostream&, const var&);
+istream& operator>>(istream&, var&);
+
+
+// Перегрузка операторов
+var operator+(const var, const var); // Оператор +
+var operator-(const var, const var); // Оператор -
+var operator*(const var, const var); // Оператор *
+var operator/(const var, const var); // Оператор /
+
+
+// Операторы сравнения
+bool operator<(const var, const var); // Оператор <
+bool operator>(const var, const var); // Оператор >
+bool operator<=(const var, const var); // Оператор <=
+bool operator>=(const var, const var); // Оператор >=
+bool operator==(const var, const var); // Оператор ==
+bool operator!=(const var, const var); // Оператор !=
